7f3b5fe204701d2499fc44952cb1acd142d1ae8c
Bruno Belotti <59092162+bbelotti@users.noreply.github.com>
2026-07-27 23:31:11 -0700

    Update push_changes.bat

--- push_changes.bat
+++ push_changes.bat
@@ -1,6 +1,10 @@
 @echo off
 cd /d "%~dp0"
 
+echo ====================================================
+echo          THE CHUBBY BABI - PULLING CHANGES          
+echo ====================================================
+echo .
 echo Checking for empty folders...
 :: This finds empty folders and creates a .gitkeep file in them to make sure empty folders are also pushed
 for /f "delims=" %%i in ('dir /ad /b /s ^| sort /r') do (
