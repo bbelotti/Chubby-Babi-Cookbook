3d7286b7f709cc8e0f1d3833b1dce4b18111a273
bbelotti <belottibruno95@gmail.com>
2026-08-02 15:11:36 -0700

    Auto-add .gitkeep to empty folders

--- publish_develop_to_main.bat
+++ publish_develop_to_main.bat
@@ -28,6 +28,15 @@ for /f "delims=" %%i in ('dir /ad /b /s ^| sort /r') do (
     )
 )
 
+echo Committing any new .gitkeep files...
+git add .
+git commit -m "Auto-add .gitkeep to empty folders"
+
+echo.
+echo Syncing local develop with remote...
+git checkout develop
+git pull origin develop
+
 echo.
 echo Switching to main and pulling latest...
 git checkout main
